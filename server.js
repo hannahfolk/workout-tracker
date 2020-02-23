dfbb3da9885c083bf8e3ae03d8aa7ceeffa43c6d
hfolk25 <hfolk25@gmail.com>
2020-02-22 23:44:53 -0800

    adding mLab

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+const password = process.env.password;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://hfolk25:${password}@ds217548.mlab.com:17548/heroku_91lf27gr`, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useMongoClient: true
 });
+
+require("dotenv").config();
 
 // routes
 app.use(require("./routes/api-routes.js"));
