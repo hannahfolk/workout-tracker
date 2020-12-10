f67ce74f75c472e23df6df223c05ab73d0f10b51
hfolk25 <hfolk25@gmail.com>
2020-12-10 09:38:37 -0800

    fixing mongoose connection

--- server.js
+++ server.js
@@ -5,19 +5,17 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const password = process.env.password;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
-
-require("dotenv").config();
 
 // routes
 app.use(require("./routes/apiRoutes.js"));
